022e98caf0f822dc73ae9e5af79de281f57fe9b3
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-07-24 16:24:27 +0300

    add filter contacts by favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,9 +2,9 @@ const { contacts } = require("../../service/models/index");
 
 const listContacts = async (req, res, next) => {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, favorite } = req.query;
     const skip = (page - 1) * limit;
-    const result = await contacts.listContacts(owner, skip, limit);
+    const result = await contacts.listContacts(owner, skip, limit, favorite);
     res.json(result);
 };
 
